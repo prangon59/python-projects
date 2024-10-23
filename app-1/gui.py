6bb1bde5a24e19a08f2726981c738c4b353c3c18
sakib <hassansakib175@gmail.com>
2024-10-24 01:42:22 +0600

    implement clock and handle errors

--- app-1/gui.py
+++ app-1/gui.py
@@ -1,6 +1,10 @@
 import functions
 import PySimpleGUI as gui
+import time
 
+gui.theme("Black")
+
+clock_label = gui.Text("", key="clock")
 label = gui.Text("Type in a to-do")
 input_box = gui.InputText(tooltip = "Enter a to-do", key="todo_input_box")
 add_button = gui.Button("Add")
@@ -13,17 +17,25 @@ complete_button = gui.Button("Complete")
 exit_button = gui.Button("Exit")
 
 window_instance = gui.Window("My To-do App", 
-                             layout = [[label], 
+                             layout = [[clock_label],
+                                       [label],
                                        [input_box, add_button], 
-                                       [todo_list, edit_button, complete_button]]
-                                       [exit_button], 
-                             font=('Helvetica', 14))
-window_instance.read()
+                                       [todo_list, edit_button, complete_button],
+                                       [exit_button]],
+                             font=('Helvetica', 10))
 
 while True:
-    event, values = window_instance.read()
-    print(event)
-    print(values)
+    event, values = window_instance.read(timeout=200)
+    # print(event)
+    # print(values)
+
+    if event == gui.WIN_CLOSED or event == "Exit":
+        break
+
+    # Only update the clock if the window is still open
+    if event != gui.WIN_CLOSED:
+        window_instance["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -33,22 +45,28 @@ while True:
             window_instance["todos_list_box"].update(values=todos)
 
         case "Edit":
-            todo_to_edit = values["todos_list_box"][0]
-            new_todo = values["todo_input_box"].strip()
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            # todos[todos.index(todo_to_edit)] = new_todo
-            todos[index] = new_todo + "\n"
-            functions.save_todos(todos)
-            window_instance["todos_list_box"].update(values=todos)
+            try:
+                todo_to_edit = values["todos_list_box"][0]
+                new_todo = values["todo_input_box"].strip()
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                # todos[todos.index(todo_to_edit)] = new_todo
+                todos[index] = new_todo + "\n"
+                functions.save_todos(todos)
+                window_instance["todos_list_box"].update(values=todos)
+            except IndexError:
+                gui.popup("Please select a to-do first!")
 
         case "Complete":
-            todo_to_complete = values["todos_list_box"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.save_todos(todos)
-            window_instance["todos_list_box"].update(values=todos)
-            window_instance["todo_input_box"].update(value="")
+            try:
+                todo_to_complete = values["todos_list_box"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.save_todos(todos)
+                window_instance["todos_list_box"].update(values=todos)
+                window_instance["todo_input_box"].update(value="")
+            except IndexError:
+                gui.popup("Please select a to-do first!")
 
         case "Exit":
             break

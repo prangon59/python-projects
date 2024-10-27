9bccf1a0447014aed45d610acda69818e4894d6c
sakib <hassansakib175@gmail.com>
2024-10-28 01:41:35 +0600

    added contact page and implement send email functionality (day 23)

---
+++ app-2-portfolio/Home.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Sakib Hassan Prangon")
+    content = '''Motivated IT professional with a foundation in cybersecurity and IT administration. Seeking a remote position in helpdesk, IT support, penetration testing or any technical fields to gain practical experience, expand skills, and contribute to a dynamic team.'''
+    st.info(content)
+
+
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")

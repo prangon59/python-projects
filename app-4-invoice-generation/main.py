a40efe3d2d7d3066dcb8d3c17d01f60c08dfa440
sakib <hassansakib175@gmail.com>
2024-10-31 15:33:28 +0600

    added invoice num and date on page (day 26)

--- app-4-invoice-generation/main.py
+++ app-4-invoice-generation/main.py
@@ -1,8 +1,23 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    filename = Path(filepath).stem
+    invoice_number, date = filename.split("-")
+
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Invoice Number {invoice_number}", ln=1)
+
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Date {invoice_number}")
+
+
+    pdf.output(f"PDFs/{filename}.pdf")

4f6daecd29b90cfa73bd038e14c802b145b358cf
sakib <hassansakib175@gmail.com>
2024-11-02 11:38:53 +0600

    add email subject, links, limit news, specify language

--- app-5-news-api-email/main.py
+++ app-5-news-api-email/main.py
@@ -7,15 +7,22 @@ from send_email import send_email
 load_dotenv()
 
 api_key = os.getenv("NEWS_API_KEY")
-url = f"https://newsapi.org/v2/everything?q=bitcoin&apiKey={api_key}"
+
+topic = "bitcoin"
+url = f"https://newsapi.org/v2/everything?q={topic}&language=en&apiKey={api_key}"
 
 request = requests.get(url)
 content = request.json()
 
-body = ""
-for articles in content["articles"]:
+body = "Subject: Today's Hot News\n\n"
+for articles in content["articles"][:10]:
     if articles["title"] is not None:
-        body = body + articles["title"] + "\n" + articles["description"] + 2*"\n"
+        body += (
+            articles["title"] + "\n" 
+            + (articles["description"] or "No description available.") + "\n"
+            + articles["url"] + "\n\n"
+        )
 
+body = body.encode("utf-8")
 send_email(body)
 

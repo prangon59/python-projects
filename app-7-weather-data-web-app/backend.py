8b25c7e1820375bfa744cc945f1b52efc6a978db
sakib <hassansakib175@gmail.com>
2024-11-08 22:21:54 +0600

    added sky conditions

--- app-7-weather-data-web-app/backend.py
+++ app-7-weather-data-web-app/backend.py
@@ -2,17 +2,13 @@ import requests
 
 API_KEY = "f83e9e90b6bc04fe761a6d50715ab2c4"
 
-def get_data(place, forecast_days=None, weather_type=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     num_of_values = 8 * forecast_days # 5 day at 3 hour interval forecast data means 24/3 = 8 data points in a day
     filtered_data = filtered_data[:num_of_values]
-    if weather_type == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if weather_type == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 

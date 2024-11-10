b71a9f3e45dd0195fd8bc672cc2dcf2a3491a1d6
sakib <hassansakib175@gmail.com>
2024-11-11 01:15:41 +0600

    send mail dummy when motion object is detected and exited the screen

--- app-9-email-webcam-detection/main.py
+++ app-9-email-webcam-detection/main.py
@@ -1,15 +1,18 @@
 import cv2
 import time
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 # time.sleep(1)
 
 first_frame = None
+status_list = []
 
 while True:
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gaussian_blur_frame = cv2.GaussianBlur(gray_frame, (21,21), 0)
+    gaussian_blur_frame = cv2.GaussianBlur(gray_frame, (81,81), 0)
 
     if first_frame is None:
         first_frame = gaussian_blur_frame
@@ -23,10 +26,20 @@ while True:
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 4000:
+        if cv2.contourArea(contour) < 10000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+        
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email()
+        
+    print(status_list)
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)

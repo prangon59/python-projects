8328082fc3f36a4fe3116e65879467b816b45ef4
sakib <hassansakib175@gmail.com>
2024-11-19 01:01:09 +0600

    implement credit card feature inheritance (day 42)

--- app-11-hotel-reservation/main.py
+++ app-11-hotel-reservation/main.py
@@ -1,6 +1,8 @@
 import pandas
 
 df = pandas.read_csv("hotels.csv", dtype={"id": str})
+df_cards = pandas.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+df_card_security = pandas.read_csv("card_security.csv", dtype=str)
 
 
 class Hotel:
@@ -9,7 +11,7 @@ class Hotel:
         self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
     def book(self):
-        '''Book a hotel bu changing its availability to no'''
+        '''Book a hotel by changing its availability to no'''
         df.loc[df["id"] == self.hotel_id, "available"] = "no"
         df.to_csv("hotels.csv", index=False)
 
@@ -20,7 +22,6 @@ class Hotel:
             return True
         else:
             return False
-
 
 
 class ReservationTicket:
@@ -37,14 +38,43 @@ class ReservationTicket:
         return content
 
 
+class CreditCard:
+    def __init__(self,number):
+        self.number = number
+    
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration, 
+        "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
+class SecureCreditCard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_card_security.loc[df_card_security["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
+
+
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+    credit_card = SecureCreditCard(number="1234")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        if credit_card.authenticate(given_password="mypass"):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("Credit card authentication failed")
+    else:
+        print("There is a problem in payment")
 else:
     print("Hotel is not free.")

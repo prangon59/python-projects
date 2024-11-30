2cc22e0df055c22cbbdb0587dc93eb7a777374fb
sakib <hassansakib175@gmail.com>
2024-12-01 01:06:43 +0600

    show success notification upon submission

--- app-16-flask-form/app.py
+++ app-16-flask-form/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, request
+from sqlite3 import IntegrityError
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "myapplication123"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "default-secret-key")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
 
@@ -12,7 +18,7 @@ class Form(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
-    email = db.Column(db.String(80))
+    email = db.Column(db.String(80), unique = True)
     date = db.Column(db.Date)
     occupation = db.Column(db.String(80))
 
@@ -24,7 +30,20 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+        
+        try:
+            db.session.add(form)
+            db.session.commit()
+            flash(f"{first_name} Form was submitted successfully", "success")
+        except IntegrityError:
+            db.session.rollback()
+            flash(f"An entry with email {email} already exists.", "danger")
+        return redirect(url_for("index"))
 
     return render_template("index.html")
 

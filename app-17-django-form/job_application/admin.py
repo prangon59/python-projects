0812782396e9b95475f76bcadfc455f3cf71a9c2
sakib <hassansakib175@gmail.com>
2024-12-03 21:34:48 +0600

    customizing the admin interface

--- app-17-django-form/job_application/admin.py
+++ app-17-django-form/job_application/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("first_name", "last_name", "email", "date", "occupation")
+    list_filter = ("first_name", "last_name", "email", "date", "occupation")
+    ordering = ("first_name", "last_name", "email", "date", "occupation")
+
+admin.site.register(Form, FormAdmin)

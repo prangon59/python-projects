b2702f7de67aba81d1bf06e9a5e76a92ae5594ac
sakib <hassansakib175@gmail.com>
2024-12-03 16:24:23 +0600

    send an email with Django

--- app-17-django-form/job_application/views.py
+++ app-17-django-form/job_application/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 def index(request):
     if request.method == "POST":
@@ -13,8 +14,24 @@ def index(request):
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
 
+            # Saveing the form data to the database
             Form.objects.create(first_name=first_name, last_name=last_name,
                                 email=email, date=date, occupation=occupation)
+
+            # Send email to the user
+            message_body = (
+                f"Thank you for submitting your application.\n"
+                f"Here is a summary of your application:\n"
+                f"Name: {first_name} {last_name}\n"
+                f"Email: {email}\n"
+                f"Occupation: {occupation}\n"
+                f"Date: {date}\n"
+                f"Thank you!"
+                )
+            email_message = EmailMessage("Form submitted", message_body, to=[email])
+            email_message.send()
+            
             messages.success(request, "Form submitted successfully!")
             return redirect('index')
+
     return render(request, "index.html")
--- app-17-django-form/mysite/settings.py
+++ app-17-django-form/mysite/settings.py
@@ -122,3 +122,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')

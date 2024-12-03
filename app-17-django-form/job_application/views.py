677113ef4fe4b3499518addab4f5fb78a68ff9c6
sakib <hassansakib175@gmail.com>
2024-12-03 14:59:29 +0600

    store the user entered form data into database table

--- app-17-django-form/job_application/views.py
+++ app-17-django-form/job_application/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
 
 def index(request):
     if request.method == "POST":
@@ -10,5 +12,9 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
-            print(first_name, last_name, occupation)
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
+            messages.success(request, "Form submitted successfully!")
+            return redirect('index')
     return render(request, "index.html")
